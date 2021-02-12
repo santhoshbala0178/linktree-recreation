b1c8a43c3e5b8ce2e0e4eb045c994dfb6a2c07a2
Sandy <santhoshbala0718@gmail.com>
2021-02-12 18:28:11 +0530

    Fixed Es list issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const HTMLWebPackPlugin = require("html-webpack-plugin");
+const HTMLWebPackPlugin = require('html-webpack-plugin');
 
 const HTMLWebPackPluginConfig = new HTMLWebPackPlugin({
-  template: __dirname + "/public/index.html",
-  filename: "index.html",
-  inject: "body",
+  template: __dirname + '/public/index.html',
+  filename: 'index.html',
+  inject: 'body',
 });
 
 module.exports = {
-  entry: __dirname + "/src/index.jsx",
+  entry: __dirname + '/src/index.jsx',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: ["style-loader", "css-loader"],
+        loader: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
@@ -34,9 +34,9 @@ module.exports = {
     historyApiFallback: true,
   },
   output: {
-    filename: "transformed.js",
-    path: __dirname + "/build",
-    publicPath: "/",
+    filename: 'transformed.js',
+    path: __dirname + '/build',
+    publicPath: '/',
   },
   plugins: [HTMLWebPackPluginConfig],
 };
